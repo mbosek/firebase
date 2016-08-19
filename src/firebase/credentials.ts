545f038666967a5112cbdff1f21cde6f0a6e23c5
Mateusz Bosek <bosek.mateusz@gmail.com>
2016-08-19 14:44:04 +0200

    Split firebase config

---
+++ src/firebase/credentials.ts
@@ -0,0 +1,6 @@
+export const FbConfig = {
+  apiKey: "AIzaSyADdt1MbGKSCRs_YYxr2_HRrMgIYmFbUMQ",
+  authDomain: "hr-opstalent.firebaseapp.com",
+  databaseURL: "https://hr-opstalent.firebaseio.com",
+  storageBucket: "hr-opstalent.appspot.com"
+};
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,17 +1,9 @@
 import { AngularFireModule, AuthMethods } from 'angularfire2';
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyADdt1MbGKSCRs_YYxr2_HRrMgIYmFbUMQ",
-  authDomain: "hr-opstalent.firebaseapp.com",
-  databaseURL: "https://hr-opstalent.firebaseio.com",
-  storageBucket: "hr-opstalent.appspot.com"
-};
+import { FbConfig } from './credentials';
 
 const firebaseAuthConfig = {
   method: AuthMethods.Popup,
   remember: 'default'
 };
 
-
-export const FirebaseModule = AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig);
+export const FirebaseModule = AngularFireModule.initializeApp(FbConfig, firebaseAuthConfig);
